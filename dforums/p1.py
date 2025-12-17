f39b6654e724b422a215ba8db75c3fb5a8102c5a
Suhana <24331a05i2@mvgrce.edu.in>
2025-12-17 22:17:59 +0530

    NumPy function for Dimension

---
+++ dforums/p1.py
@@ -0,0 +1,9 @@
+import numpy as np
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([[1, 2], [3, 4]])
+print("Array 1:", arr1)
+print("Array 2:\n", arr2)
+print("Dimensions of arr1:", np.ndim(arr1))
+print("Dimensions of arr2:", np.ndim(arr2))
+vector1 = np.array([1, 2, 3])
+vector2 = np.array([4, 5, 6])
