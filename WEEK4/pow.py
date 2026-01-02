6b5a49912b64cb1bbaa488a58f14b081001c75f1
Suhana <24331a05i2@mvgrce.edu.in>
2026-01-02 06:22:53 +0530

    Control Statements

--- WEEK4/pow.py
+++ WEEK4/pow.py
@@ -1,9 +1,14 @@
-base = int(input("Enter base: "))
-exp = int(input("Enter exponent: "))
+def Power(base, exp):
+    if exp < 0:
+        print("Power should not be negative")
+    elif exp == 0:
+        print(base, "^", exp, "=", 1)
+    else:
+        result = 1
+        for i in range(exp):
+            result = result * base
+        print(base, "^", exp, "=", result)
 
-result = 1
-
-for i in range(exp):
-    result = result * base
-
-print("Result:", result)
+a = int(input("Enter base: "))
+b = int(input("Enter power value: "))
+Power(a, b)
