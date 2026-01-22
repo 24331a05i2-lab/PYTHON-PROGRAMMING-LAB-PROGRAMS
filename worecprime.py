ee6251ff8a1bff45d191d16604c8b7e69f21d678
Suhana <24331a05i2@mvgrce.edu.in>
2026-01-22 22:33:35 +0530

    Programs on Functions

---
+++ worecprime.py
@@ -0,0 +1,13 @@
+import math
+
+num = int(input("Enter a number: "))
+
+if num <= 1:
+    print("Not a Prime Number")
+else:
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            print("Not a Prime Number")
+            break
+    else:
+        print("Prime Number")
