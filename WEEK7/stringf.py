c44c30aad219c5c860b7524926fcad779d124f19
Suhana <24331a05i2@mvgrce.edu.in>
2026-01-27 05:58:58 +0530

    Programs on Strings and Lists

---
+++ WEEK7/stringf.py
@@ -0,0 +1,6 @@
+str="Ameer Khan"
+print("Asli wala string: ",str)
+print("Uppercase wala string: ",str.upper())
+print("Lowercase wala string: ",str.lower())
+print("Replaced wala string: ",str.replace("Ameer","Suhana"))
+print("Dhuudne wala string: ",str.find("a"))
